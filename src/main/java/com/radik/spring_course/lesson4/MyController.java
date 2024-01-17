987cc0694f1ec9d3697554e71068056175f7ca9a
Radik Akhmetgareev <driveclub228@gmail.com>
2024-01-17 19:01:58 +0300

    lesson1

---
+++ src/main/java/com/radik/spring_course/lesson4/MyController.java
@@ -0,0 +1,12 @@
+package com.radik.spring_course.lesson4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping("/hello_world")
+    public String sayHello(){
+        return "hello";
+    }
+}
