c380479e9d5819de0f3ecf35726b9113b0dd298d
Radik Akhmetgareev <driveclub228@gmail.com>
2024-03-22 15:21:43 +0300

    lesson37 rest + java конфигурация

---
+++ src/main/java/com/radik/spring_course/lesson37/configuration/MyWebInitializer.java
@@ -0,0 +1,21 @@
+package com.radik.spring_course.lesson37.configuration;
+
+import com.sun.tools.javac.code.Attribute;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
